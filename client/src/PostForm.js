e22bce164a00e6e637ed65835336793f778f22ea
Sita Bérété <sita.berete.3@gmail.com>
2022-07-30 17:04:54 +0000

    [FE] improve form ux

--- client/src/PostForm.js
+++ client/src/PostForm.js
@@ -1,27 +1,43 @@
 import React, { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 
 const PostForm = () => {
   const [title, setTitle] = useState("");
+  const [isSubmitting, setSubmitting] = useState(false);
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    await axios.post('http://localhost:4000/posts', {title});
-    setTitle('');
+    setSubmitting(true);
+    await axios.post("http://localhost:4000/posts", { title });
+    setTitle("");
+    setSubmitting(false);
   };
 
+  let Button;
+  if (isSubmitting) {
+    Button = (
+      <div class="spinner-border text-primary" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    );
+  } else {
+    Button = <button className="btn btn-primary">Submit</button>;
+  }
+
   return (
-    <div className="col-md-6">
+    <div className="col-md-6 no-gutters">
       <form onSubmit={onSubmit}>
         <div className="form-group">
           <label>Title</label>
           <input
+            value={title}
             type={"text"}
             onChange={(e) => setTitle(e.target.value)}
             className="form-control"
+            required
           />
         </div>
-        <button className="btn btn-primary">Submit</button>
+        {Button}
       </form>
     </div>
   );
