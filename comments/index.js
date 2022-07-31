316178f5e8fea4a58bab7ece50600a5634eb56df
Sita Bérété <sita.berete.3@gmail.com>
2022-07-31 16:26:43 +0000

    [BE] fix comment service urls and update port

--- comments/index.js
+++ comments/index.js
@@ -1,29 +1,29 @@
-const Express = require('express');
-const { randomBytes } = require('crypto');
-const cors = require('cors');
+const Express = require("express");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
 
 const app = Express();
-const commentsByPostId = {}
+const commentsByPostId = {};
 
 app.use(Express.json());
 app.use(cors());
 
-app.post('posts/:id/comments', (request, response) => {
-  const commentId = randomBytes(8).toString('hex');
+app.post("/posts/:id/comments", (request, response) => {
+  const commentId = randomBytes(8).toString("hex");
   const postId = request.query.id;
-  comments.push({id: commentId, content: request.body.content});
-
   const comments = commentsByPostId[postId] || [];
-  commentsByPostId[postId] = comments
+  
+  comments.push({ id: commentId, content: request.body.content });
+  commentsByPostId[postId] = comments;
 
   response.status(201).send(commentId);
 });
 
-app.get('posts/:id/comments', (request, response) => {
+app.get("/posts/:id/comments", (request, response) => {
   const postId = request.query.id;
   response.send(commentsByPostId[postId] || []);
 });
 
-app.listen(4001, () => {
-  console.log('Listening on port 4001');
-})
+app.listen(5000, () => {
+  console.log("Listening on port 5000");
+});
